60746a720cbe381bd19663242f9fbf374e21d621
KimJaeHwan <lazysoul14@gmail.com>
2021-09-29 21:26:17 +0900

    정렬 문제풀이

--- Part5_Algorithm/Chapter-17_Sort/problem_59.py
+++ Part5_Algorithm/Chapter-17_Sort/problem_59.py
@@ -2,6 +2,8 @@
 [medium] 56. Merge Intervals
 https://leetcode.com/problems/merge-intervals/
 """
+from typing import List
+
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
@@ -22,6 +24,7 @@ class Solution:
                 answer.append(interval)
         return answer
 
+
 """
 기본적인 로직은 이렇다
 1. 앞 list[1]와 뒤 list[0]를 비교한다.
@@ -35,6 +38,7 @@ class Solution:
 게다가 intervals.sort()로 정렬은 되지만 책에는 첫번쨰 원소를 기준으로 하는 것을 보면 더 안전한듯 보인다.
 """
 
+
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort(key=lambda x: x[0])
@@ -44,8 +48,8 @@ class Solution:
                 answer[-1][1] = max(answer[-1][1], interval[1])
             else:
                 answer += interval,
-
         return answer
+
 
 """
 가독성이 그렇게 떨어지지 않는 범위에서 책의 내용을 이용해 리팩토링해봤다.
@@ -68,3 +72,11 @@ a += [b]
 
 이렇게 된다고 한다.
 """
+
+"""
+다시 풀면서 확인한 점
+
+1. 사실 answer += interval, 이렇게 연산을 할 이유는 없다고 본다. answer.append(interval) 이렇게 하거나 차라리 answer += [interval] 로 코드를 작성하는 것이 더 낫아보인다.
+2. 내가 풀이한 첫번째 풀이와 책에서 풀이한 두번째 풀이는 같다. 결국 앞 interval[1] 와 뒤 앞 interval[0] 을 비교하는 건데 첫번째 풀이는 너무 복잡하게 생각했다. 병합 여부만 판단하고 병합해야한다면 앞 interval[1] 값만 변경하면 된다. 
+"""
+
