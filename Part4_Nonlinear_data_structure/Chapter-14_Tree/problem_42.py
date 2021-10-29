8581909fafae9d57044980d013f4ea286a6aa153
KimJaeHwan <lazysoul14@gmail.com>
2021-10-29 23:21:33 +0900

    트리 문제풀이

--- Part4_Nonlinear_data_structure/Chapter-14_Tree/problem_42.py
+++ Part4_Nonlinear_data_structure/Chapter-14_Tree/problem_42.py
@@ -40,3 +40,33 @@ if node.left:  &  if node.right:
 for node in queue 이 아닌 for _ in range(len(queue)) 를 사용한 이유?
 -> iteration 시 queue 에 값을 추가하여 문제가 생긴다. 설령 문제가 생기지 않더라도 각 depth 의 노드들만 순회해야하므로 현재 큐에 있는 노드들만 조회해야 한다.
 """
+
+
+"""
+DFS 풀이
+"""
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        def dfs(node):
+            if not node:
+                return -1
+
+            left_height = dfs(node.left)
+            right_height = dfs(node.right)
+
+            return max(left_height, right_height) + 1
+
+        depth = dfs(root)
+        return depth + 1
+
+
+"""
+BFS 보다 DFS 가 더 간결하게 풀이 가능해보여 시도해보았다.
+사실 DFS 는 height 를 구하는 풀이 방법이고 이 문제는 depth 를 구하는 문제이기 때문에 BFS 를 사용하는 것 맞다.
+다만 해결방법은 비슷하니 풀이해보았다.
+"""
